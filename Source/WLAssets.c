2f458c6504d4531365985d3bf17bee5dd606dc71
Israfil <174603598+israfiel-a@users.noreply.github.com>
2025-06-22 13:19:04 -0400

    updated names for new project setup

--- Source/WLAssets.c
+++ Source/WLAssets.c
@@ -1,13 +1,13 @@
-#include <TKAssets.h>
+#include <WLAssets.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-bool tkast_loadFile(tkast_file_t *file)
+bool waterlily_loadFile(waterlily_file_t *file)
 {
-    char *path = malloc(tkast_maxPathLength);
-    snprintf(path, tkast_maxPathLength, "%s/%s/%s%s", tkast_baseDirectory,
-             tkast_getDirectory(file->type), file->name,
-             tkast_getExtension(file->type));
+    char *path = malloc(waterlily_maxPathLength);
+    snprintf(path, waterlily_maxPathLength, "%s/%s/%s%s",
+             waterlily_baseDirectory, waterlily_getDirectory(file->type),
+             file->name, waterlily_getExtension(file->type));
 
     FILE *fileHandle = fopen(path, "r");
     if (fileHandle == nullptr)
@@ -30,12 +30,12 @@ bool tkast_loadFile(tkast_file_t *file)
     return true;
 }
 
-bool tkast_writeFile(tkast_file_t *file, const uint8_t *contents)
+bool waterlily_writeFile(waterlily_file_t *file, const uint8_t *contents)
 {
-    char *path = malloc(tkast_maxPathLength);
-    snprintf(path, tkast_maxPathLength, "%s/%s/%s%s", tkast_baseDirectory,
-             tkast_getDirectory(file->type), file->name,
-             tkast_getExtension(file->type));
+    char *path = malloc(waterlily_maxPathLength);
+    snprintf(path, waterlily_maxPathLength, "%s/%s/%s%s",
+             waterlily_baseDirectory, waterlily_getDirectory(file->type),
+             file->name, waterlily_getExtension(file->type));
 
     FILE *fileHandle = fopen(path, "w");
     if (fileHandle == nullptr)
---
+++ WLAssets.h
@@ -0,0 +1,56 @@
+#ifndef WLASSETS_MAIN_H
+#define WLASSETS_MAIN_H
+
+#define __need_size_t
+#include <stddef.h>
+#include <stdint.h>
+
+typedef enum
+{
+    WATERLILY_TEXT_FILE,
+    WATERLILY_SPIRV_FILE,
+    WATERLILY_VERTEX_FILE,
+    WATERLILY_FRAGMENT_FILE,
+} waterlily_file_type_t;
+
+typedef struct
+{
+    const char *name;
+    // size+1 (term. NUL)
+    uint8_t *content;
+    size_t size;
+    waterlily_file_type_t type;
+} waterlily_file_t;
+
+static const size_t waterlily_maxPathLength = 512;
+static const char *const waterlily_baseDirectory = "./Resources";
+
+inline const char *waterlily_getExtension(waterlily_file_type_t type)
+{
+    switch (type)
+    {
+        case WATERLILY_SPIRV_FILE:    return ".spv";
+        case WATERLILY_VERTEX_FILE:   return ".vert";
+        case WATERLILY_FRAGMENT_FILE: return ".frag";
+        case WATERLILY_TEXT_FILE:     [[fallthrough]];
+        default:                      return ".txt";
+    }
+}
+
+inline const char *waterlily_getDirectory(waterlily_file_type_t type)
+{
+    switch (type)
+    {
+        case WATERLILY_VERTEX_FILE:   [[fallthrough]];
+        case WATERLILY_FRAGMENT_FILE: [[fallthrough]];
+        case WATERLILY_SPIRV_FILE:    return "Shaders";
+        case WATERLILY_TEXT_FILE:     [[fallthrough]];
+        default:                      return ".";
+    }
+}
+
+bool waterlily_loadFile(waterlily_file_t *file);
+
+bool waterlily_writeFile(waterlily_file_t *file, const uint8_t *contents);
+
+#endif // WLASSETS_MAIN_H
