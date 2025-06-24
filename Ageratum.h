57c0a6b770790daaacbc949feefb0067138e8ed5
Israfil <174603598+israfiel-a@users.noreply.github.com>
2025-06-24 01:07:51 -0400

    renamed all symbols

--- Ageratum.h
+++ Ageratum.h
@@ -1,5 +1,5 @@
-#ifndef WLASSETS_MAIN_H
-#define WLASSETS_MAIN_H
+#ifndef AGERATUM_H
+#define AGERATUM_H
 
 #define __need_size_t
 #include <stddef.h>
@@ -7,11 +7,11 @@
 
 typedef enum
 {
-    WATERLILY_TEXT_FILE,
-    WATERLILY_SPIRV_FILE,
-    WATERLILY_VERTEX_FILE,
-    WATERLILY_FRAGMENT_FILE,
-} waterlily_file_type_t;
+    AGERATUM_TEXT_FILE,
+    AGERATUM_SPIRV_FILE,
+    AGERATUM_VERTEX_FILE,
+    AGERATUM_FRAGMENT_FILE,
+} ageratum_file_type_t;
 
 typedef struct
 {
@@ -19,51 +19,51 @@ typedef struct
     // size+1 (term. NUL)
     uint8_t *content;
     size_t size;
-    waterlily_file_type_t type;
-} waterlily_file_t;
+    ageratum_file_type_t type;
+} ageratum_file_t;
 
-static const size_t waterlily_maxPathLength = 512;
-static const char *const waterlily_baseDirectory = "./Resources";
+static const size_t ageratum_maxPathLength = 512;
+static const char *const ageratum_baseDirectory = "./Resources";
 
-inline const char *waterlily_getExtension(waterlily_file_type_t type)
+inline const char *ageratum_getExtension(ageratum_file_type_t type)
 {
     switch (type)
     {
-        case WATERLILY_SPIRV_FILE:    return ".spv";
-        case WATERLILY_VERTEX_FILE:   return ".vert";
-        case WATERLILY_FRAGMENT_FILE: return ".frag";
-        case WATERLILY_TEXT_FILE:     [[fallthrough]];
-        default:                      return ".txt";
+        case AGERATUM_SPIRV_FILE:    return ".spv";
+        case AGERATUM_VERTEX_FILE:   return ".vert";
+        case AGERATUM_FRAGMENT_FILE: return ".frag";
+        case AGERATUM_TEXT_FILE:     [[fallthrough]];
+        default:                     return ".txt";
     }
 }
 
-inline const char *waterlily_getDirectory(waterlily_file_type_t type)
+inline const char *ageratum_getDirectory(ageratum_file_type_t type)
 {
     switch (type)
     {
-        case WATERLILY_VERTEX_FILE:   [[fallthrough]];
-        case WATERLILY_FRAGMENT_FILE: [[fallthrough]];
-        case WATERLILY_SPIRV_FILE:    return "Shaders";
-        case WATERLILY_TEXT_FILE:     [[fallthrough]];
-        default:                      return ".";
+        case AGERATUM_VERTEX_FILE:   [[fallthrough]];
+        case AGERATUM_FRAGMENT_FILE: [[fallthrough]];
+        case AGERATUM_SPIRV_FILE:    return "Shaders";
+        case AGERATUM_TEXT_FILE:     [[fallthrough]];
+        default:                     return ".";
     }
 }
 
-bool waterlily_loadFile(waterlily_file_t *file);
+bool ageratum_loadFile(ageratum_file_t *file);
 
-bool waterlily_writeFile(waterlily_file_t *file, const uint8_t *contents);
+bool ageratum_writeFile(ageratum_file_t *file, const uint8_t *contents);
 
-#ifdef WLASSETS_IMPLEMENTATION
+#ifdef AGERATUM_IMPLEMENTATION
 
 #include <stdio.h>
 #include <stdlib.h>
 
-bool waterlily_loadFile(waterlily_file_t *file)
+bool ageratum_loadFile(ageratum_file_t *file)
 {
-    char *path = malloc(waterlily_maxPathLength);
-    snprintf(path, waterlily_maxPathLength, "%s/%s/%s%s",
-             waterlily_baseDirectory, waterlily_getDirectory(file->type),
-             file->name, waterlily_getExtension(file->type));
+    char *path = malloc(ageratum_maxPathLength);
+    snprintf(path, ageratum_maxPathLength, "%s/%s/%s%s", ageratum_baseDirectory,
+             ageratum_getDirectory(file->type), file->name,
+             ageratum_getExtension(file->type));
 
     FILE *fileHandle = fopen(path, "r");
     if (fileHandle == nullptr)
@@ -86,12 +86,12 @@ bool waterlily_loadFile(waterlily_file_t *file)
     return true;
 }
 
-bool waterlily_writeFile(waterlily_file_t *file, const uint8_t *contents)
+bool ageratum_writeFile(ageratum_file_t *file, const uint8_t *contents)
 {
-    char *path = malloc(waterlily_maxPathLength);
-    snprintf(path, waterlily_maxPathLength, "%s/%s/%s%s",
-             waterlily_baseDirectory, waterlily_getDirectory(file->type),
-             file->name, waterlily_getExtension(file->type));
+    char *path = malloc(ageratum_maxPathLength);
+    snprintf(path, ageratum_maxPathLength, "%s/%s/%s%s", ageratum_baseDirectory,
+             ageratum_getDirectory(file->type), file->name,
+             ageratum_getExtension(file->type));
 
     FILE *fileHandle = fopen(path, "w");
     if (fileHandle == nullptr)
@@ -110,4 +110,4 @@ bool waterlily_writeFile(waterlily_file_t *file, const uint8_t *contents)
 
 #endif
 
-#endif // WLASSETS_MAIN_H
+#endif // AGERATUM_H
